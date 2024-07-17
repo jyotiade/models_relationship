2a97c5556093cd08760e597b0bf2c9feff969149
jyoti ade <jyotiade287@gmail.com>
2024-07-17 20:52:51 +0530

    models relationship

---
+++ project/app/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+# from .models import *
+from .models import Aadhar,Student
+# Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Aadhar)
---
+++ project/app/models.py
@@ -0,0 +1,22 @@
+from django.db import models
+
+# Create your models here.
+
+class Aadhar(models.Model):
+    aadhar_no=models.IntegerField(unique=True)
+    created_no=models.DateTimeField()
+    created_by=models.CharField(max_length=50)
+
+    def __str__(self):
+        # return self.aadhar_no
+        return str(self.aadhar_no)
+
+class Student(models.Model):
+    stu_name=models.CharField(max_length=50)
+    stu_city=models.CharField(max_length=50)
+    stu_email=models.EmailField()
+    aadhar_no=models.OneToOneField(Aadhar,on_delete=models.CASCADE)
+     
+    def __str__(self):
+        return self.stu_name
+        
